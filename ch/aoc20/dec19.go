4c38f8238ee86f5e0281c8c24f429def0e2f5f36
Thijs van Dijk <github@inurbanus.nl>
2021-12-11 13:34:37 +0100

    Implement the CYK algorithm

--- ch/aoc20/dec19.go
+++ ch/aoc20/dec19.go
@@ -10,16 +10,10 @@ import (
 )
 
 func Dec19a(ctx ch.AOContext) error {
-	rset, lines, err := parseRuleSet(ctx, "inputs/2020/dec19.txt")
+	rset, lines, err := readRuleSet(ctx, "inputs/2020/dec19.txt")
 	if err != nil {
 		return err
 	}
-
-	ctx.Printf("Rules:")
-	for id, rule := range rset {
-		ctx.Printf("   %3d : %v", id, rule)
-	}
-	ctx.Printf("Strings: %s", lines)
 
 	rv := 0
 	for i, line := range lines {
@@ -38,6 +32,51 @@ func Dec19a(ctx ch.AOContext) error {
 }
 
 func Dec19b(ctx ch.AOContext) error {
+	rset, err := parseRuleSet([]string{
+		"0: 1 2",
+		"1: 2 2 | \"a\"",
+		"2: 1 2 | \"b\"",
+	})
+	if err != nil {
+		return err
+	}
+	ctx.Printf("Rules: (CNF: %v)", rset.IsChomskyNormalForm())
+	for id, rule := range rset {
+		ctx.Printf("   %3d : %v", id, rule)
+	}
+
+	for _, s := range []string{"aabbb", "abb", "bbb", "abba", "aabba", "abbbb"} {
+		ctx.Printf("Matches '%s': %v", s, rset.CYKMatch(s, 0))
+	}
+
+	return fmt.Errorf("stopping here for now")
+
+	rset, lines, err := readRuleSet(ctx, "inputs/2020/dec19b.txt")
+	if err != nil {
+		return err
+	}
+
+	rset[8] = []grammarRule{
+		{grammarLexeme{RuleID: 42}, grammarLexeme{RuleID: 8}},
+		{grammarLexeme{RuleID: 42}},
+	}
+	rset[11] = []grammarRule{
+		{grammarLexeme{RuleID: 42}, grammarLexeme{RuleID: 11}, grammarLexeme{RuleID: 31}},
+		{grammarLexeme{RuleID: 42}, grammarLexeme{RuleID: 31}},
+	}
+
+	rv := 0
+	for i, line := range lines {
+		if line == "" {
+			continue
+		}
+		if rset.Match(line, 0) {
+			ctx.Printf("%3d: match  %s", i+1, line)
+			rv++
+		}
+	}
+
+	ctx.FinalAnswer.Print(rv)
 	return errors.New("not implemented")
 }
 
@@ -50,26 +89,43 @@ type grammarRule []grammarLexeme
 
 type grammarRuleSet map[int][]grammarRule
 
-func parseRuleSet(ctx ch.AOContext, assetName string) (grammarRuleSet, []string, error) {
+func readRuleSet(ctx ch.AOContext, assetName string) (grammarRuleSet, []string, error) {
 	lines, err := ctx.DataLines(assetName)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	rv := make(grammarRuleSet)
+	rules := lines
+	var rest []string = nil
 
 	for i, l := range lines {
 		if l == "" {
-			return rv, lines[i+1:], nil
+			rules = lines[:i]
+			rest = lines[i+1:]
+			break
+		}
+	}
+
+	rv, err := parseRuleSet(rules)
+	return rv, rest, err
+}
+
+func parseRuleSet(rules []string) (grammarRuleSet, error) {
+	rv := make(grammarRuleSet)
+
+	for i, l := range rules {
+		if l == "" {
+			continue
 		}
 
 		idparts := strings.SplitN(l, ": ", 2)
 		if len(idparts) != 2 {
-			return nil, nil, fmt.Errorf("error on line %d: '%s'", i+1, l)
+			return nil, fmt.Errorf("error on line %d: '%s'", i+1, l)
 		}
 		var id int
+		var err error
 		if id, err = strconv.Atoi(idparts[0]); err != nil {
-			return nil, nil, err
+			return nil, err
 		}
 
 		options := strings.Split(idparts[1], " | ")
@@ -87,7 +143,31 @@ func parseRuleSet(ctx ch.AOContext, assetName string) (grammarRuleSet, []string,
 		}
 	}
 
-	return rv, nil, nil
+	return rv, nil
+}
+
+func (rset grammarRuleSet) IsChomskyNormalForm() bool {
+	for _, options := range rset {
+		for _, opt := range options {
+			if len(opt) == 0 {
+				return false
+			} else if len(opt) == 1 {
+				if len(opt[0].Literal) != 1 {
+					return false
+				}
+			} else if len(opt) == 2 {
+				if opt[0].Literal != "" || opt[1].Literal != "" {
+					return false
+				}
+				if len(rset[opt[0].RuleID]) == 0 || len(rset[opt[1].RuleID]) == 0 {
+					return false
+				}
+			} else {
+				return false
+			}
+		}
+	}
+	return true
 }
 
 func (rset grammarRuleSet) Match(str string, initial int) bool {
@@ -126,4 +206,59 @@ func (rset grammarRuleSet) matchRule(str string, offset int, ruleID int, depth i
 	}
 
 	return 0, false
+}
+
+func (rset grammarRuleSet) CYKMatch(str string, initial int) bool {
+	V := make([][]map[int]bool, len(str))
+
+	for l := range str {
+		V[l] = make([]map[int]bool, len(str)-l)
+		if l == 0 {
+			for i, c := range str {
+				V[l][i] = make(map[int]bool)
+				for id, opts := range rset {
+					for _, option := range opts {
+						if len(option) == 1 && len(option[0].Literal) == 1 && rune(option[0].Literal[0]) == c {
+							V[l][i][id] = true
+						}
+					}
+				}
+			}
+		} else {
+			for i := range str[l:] {
+				V[l][i] = make(map[int]bool)
+			}
+			for d := 0; d < l; d++ {
+				for j := range V[l] {
+					m := V[d][j]
+					mm := V[l-d-1][j+d+1]
+					for left := range m {
+						for right := range mm {
+							for id, opts := range rset {
+								for _, option := range opts {
+									if len(option) == 2 && option[0].Literal == "" && option[1].Literal == "" && option[0].RuleID == left && option[1].RuleID == right {
+										V[l][j][id] = true
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		// fmt.Printf("  [")
+		// for _, m := range V[l] {
+		// 	fmt.Printf(" {")
+		// 	sep := ""
+		// 	for id := range m {
+		// 		fmt.Printf("%s%d", sep, id)
+		// 		sep = " "
+		// 	}
+		// 	fmt.Printf("}")
+		// }
+		// fmt.Printf(" ]\n")
+	}
+
+	return V[len(str)-1][0][initial]
 }
