e86dcbc324a95396e465bb318b28fc3dde32e1f2
Thijs van Dijk <github@inurbanus.nl>
2021-12-17 06:48:44 +0100

    Solve 2021-17-B

--- ch/aoc21/dec17.go
+++ ch/aoc21/dec17.go
@@ -4,8 +4,6 @@ import (
 	"github.com/thijzert/advent-of-code/ch"
 )
 
-var Dec17b ch.AdventFunc = nil
-
 func Dec17a(ctx ch.AOContext) error {
 	exampleTarget := rect{point{20, -10}, point{30, -5}}
 
@@ -14,18 +12,28 @@ func Dec17a(ctx ch.AOContext) error {
 		ctx.Printf("  - %d: hits: %5v; height %d", vel, hits, height)
 	}
 
-	ctx.Printf("Example data: max height %d", fireProbeWithStyle(exampleTarget))
+	_, my := fireProbeWithStyle(exampleTarget)
+	ctx.Printf("Example data: max height %d", my)
 
 	actualTarget := rect{point{138, -125}, point{184, -71}}
-	ctx.FinalAnswer.Print(fireProbeWithStyle(actualTarget))
+	_, my = fireProbeWithStyle(actualTarget)
+	ctx.FinalAnswer.Print(my)
 
 	return nil
 }
 
-//
-// func Dec17b(ctx ch.AOContext) error {
-// 	return errNotImplemented
-// }
+func Dec17b(ctx ch.AOContext) error {
+	exampleTarget := rect{point{20, -10}, point{30, -5}}
+
+	nh, _ := fireProbeWithStyle(exampleTarget)
+	ctx.Printf("Example data: trajectories that hit: %d", nh)
+
+	actualTarget := rect{point{138, -125}, point{184, -71}}
+	nh, _ = fireProbeWithStyle(actualTarget)
+	ctx.FinalAnswer.Print(nh)
+
+	return nil
+}
 
 func fireProbe(velocity point, target rect) (bool, int) {
 	ymax := 0
@@ -45,15 +53,17 @@ func fireProbe(velocity point, target rect) (bool, int) {
 	return false, 0
 }
 
-func fireProbeWithStyle(target rect) int {
+func fireProbeWithStyle(target rect) (int, int) {
+	nHits := 0
 	maxHeight := 0
 	for vy := target.Min.Y; vy <= target.Max.X; vy++ {
 		for vx := 0; vx <= target.Max.X; vx++ {
 			hits, height := fireProbe(point{vx, vy}, target)
 			if hits {
+				nHits++
 				maxHeight = max(maxHeight, height)
 			}
 		}
 	}
-	return maxHeight
+	return nHits, maxHeight
 }
