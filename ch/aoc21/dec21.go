0f1064e8e23fb78c51535a3fa6be71e1c048fa32
Thijs van Dijk <github@inurbanus.nl>
2021-12-21 08:50:42 +0100

    Solve 2021-21-A

--- ch/aoc21/dec21.go
+++ ch/aoc21/dec21.go
@@ -4,19 +4,55 @@ import (
 	"github.com/thijzert/advent-of-code/ch"
 )
 
-var Dec21a, Dec21b ch.AdventFunc = nil, nil
+var Dec21b ch.AdventFunc = nil
 
-// func Dec21a(ctx ch.AOContext) error {
-// 	sections, err := ctx.DataSections("inputs/2021/dec21a.txt")
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	ctx.Print(len(sections))
-// 	return errNotImplemented
-// }
-//
+func Dec21a(ctx ch.AOContext) error {
+	// playerPos := []int{4, 8}
+	playerPos := []int{2, 5}
+	playerScore := []int{0, 0}
+
+	var die DeterministicDice = &DetD100{}
+
+	playing := true
+	for playing {
+		for i := range playerPos {
+			move := die.Next() + die.Next() + die.Next()
+			playerPos[i] = 1 + ((playerPos[i] + move - 1) % 10)
+			playerScore[i] += playerPos[i]
+			//ctx.Printf("Player %d rolls %d and moves to %d, bringing their score to %d", i+1, move, playerPos[i], playerScore[i])
+			if playerScore[i] >= 1000 {
+				playing = false
+				break
+			}
+		}
+	}
+
+	ctx.FinalAnswer.Print(die.NRolls() * min(playerScore...))
+	return nil
+}
+
 // func Dec21b(ctx ch.AOContext) error {
 // 	return errNotImplemented
 // }
 
+type DeterministicDice interface {
+	Next() int
+	NRolls() int
+}
+
+type DetD100 struct {
+	current int
+	nRolls  int
+}
+
+func (d *DetD100) Next() int {
+	d.current++
+	d.nRolls++
+	rv := d.current
+	d.current %= 100
+	return rv
+}
+
+func (d *DetD100) NRolls() int {
+	return d.nRolls
+}
