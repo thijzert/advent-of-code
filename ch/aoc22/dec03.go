c4dc59386a0826dcb3e11266e07abea202decb0e
Thijs van Dijk <github@inurbanus.nl>
2022-12-03 10:22:49 +0100

    Solve 2022-03-B

--- ch/aoc22/dec03.go
+++ ch/aoc22/dec03.go
@@ -11,26 +11,8 @@ func Dec03a(ctx ch.AOContext) error {
 	if err != nil {
 		return err
 	}
-	// lines = []string{
-	// 	"vJrwpWtwJgWrhcsFMMfFFhFp",
-	// 	"jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
-	// 	"PmmdzqPrVvPwwTWBwg",
-	// 	"wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
-	// 	"ttgJtRGJQctTZtZT",
-	// 	"CrZsJsPPZsGzwwsLwLmpwMDw",
-	// }
 
 	sumPr := 0
-
-	prio := func(b byte) int {
-		if b >= 'a' && b <= 'z' {
-			return int(1 + b - 'a')
-		} else if b >= 'A' && b <= 'Z' {
-			return int(27 + b - 'A')
-		}
-		return 0
-	}
-
 	for _, line := range lines {
 		cutoff := len(line) / 2
 		front, back := line[:cutoff], line[cutoff:]
@@ -41,7 +23,7 @@ func Dec03a(ctx ch.AOContext) error {
 		}
 		m := r.FindString(back)
 		ctx.Printf("both compartments contain '%s'", m)
-		sumPr += prio(m[0])
+		sumPr += rucksackPriority(m[0])
 	}
 
 	ctx.FinalAnswer.Print(sumPr)
@@ -49,5 +31,44 @@ func Dec03a(ctx ch.AOContext) error {
 }
 
 func Dec03b(ctx ch.AOContext) error {
-	return errNotImplemented
+	lines, err := ctx.DataLines("inputs/2022/dec03.txt")
+	if err != nil {
+		return err
+	}
+
+	sumPr := 0
+
+	elfIdx := byte(2)
+	seen := [53]byte{}
+	for _, line := range lines {
+		elfIdx++
+		if elfIdx == 3 {
+			for i := range seen {
+				seen[i] = 0
+			}
+			elfIdx = 0
+		}
+		for _, c := range line {
+			i := rucksackPriority(byte(c))
+			if seen[i] == elfIdx {
+				seen[i]++
+				if seen[i] == 3 {
+					ctx.Printf("All elves carry '%c'", c)
+					sumPr += i
+				}
+			}
+		}
+	}
+
+	ctx.FinalAnswer.Print(sumPr)
+	return nil
+}
+
+func rucksackPriority(b byte) int {
+	if b >= 'a' && b <= 'z' {
+		return int(1 + b - 'a')
+	} else if b >= 'A' && b <= 'Z' {
+		return int(27 + b - 'A')
+	}
+	return 0
 }
