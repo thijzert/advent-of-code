4490ff296a2cd921d1d7366161930fa464fdf59a
Thijs van Dijk <github@inurbanus.nl>
2022-12-03 10:34:34 +0100

    Add a more or less reusable library for finding the shortest path in some arbitrary network. (And solve 2019-18-A)

--- ch/aoc19/dec18.go
+++ ch/aoc19/dec18.go
@@ -1,23 +1,170 @@
 package aoc19
 
 import (
+	"strings"
+
 	"github.com/thijzert/advent-of-code/ch"
+	"github.com/thijzert/advent-of-code/lib/dijkstra"
 )
 
-var Dec18a ch.AdventFunc = nil
 var Dec18b ch.AdventFunc = nil
 
-// func Dec18a(ctx ch.AOContext) error {
-// 	sections, err := ctx.DataSections("inputs/2019/dec18a.txt")
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	ctx.Print(len(sections))
-// 	return errNotImplemented
-// }
-//
+func Dec18a(ctx ch.AOContext) error {
+	sections, err := ctx.DataSections("inputs/2019/dec18.txt")
+	if err != nil {
+		return err
+	}
+
+	for _, lines := range sections {
+		tm := tractorMaze{Lines: lines}
+		kreq := make([]byte, 26)
+		kl := 0
+		for _, l := range lines {
+			for _, c := range l {
+				if c >= 'a' && c <= 'z' {
+					cc := int(c - 'a')
+					if kl <= cc {
+						kl = cc + 1
+					}
+					kreq[cc] = byte(c)
+				}
+			}
+		}
+		tm.KeyRequirement = string(kreq[:kl])
+
+		ctx.Printf("[%s]", tm.KeyRequirement)
+
+		_, cost, err := dijkstra.ShortestPath(tm)
+		if err != nil {
+			return err
+		}
+		ctx.FinalAnswer.Print(cost)
+	}
+	return nil
+}
+
+type tractorMaze struct {
+	Lines          []string
+	KeyRequirement string
+}
+
+func (b tractorMaze) StartingPositions() []dijkstra.Position {
+	defaultKeys := strings.Repeat(" ", len(b.KeyRequirement))
+	rv := []dijkstra.Position{}
+	for y, l := range b.Lines {
+		for x, c := range l {
+			if c == '@' {
+				rv = append(rv, pos2d{x, y, defaultKeys})
+			}
+		}
+	}
+	return rv
+}
+
+func (b tractorMaze) charAt(x, y int) rune {
+	if y >= 0 && y < len(b.Lines) {
+		if x >= 0 && x < len(b.Lines[y]) {
+			return rune(b.Lines[y][x])
+		}
+	}
+	return '#'
+}
+
+func (b tractorMaze) posAt(x, y int, currentKeys string) pos2d {
+	c := b.charAt(x, y)
+	if c >= 'a' && c <= 'z' {
+		i := int(c - 'a')
+		if currentKeys[i] != byte(c) {
+			currentKeys = currentKeys[:i] + string(c) + currentKeys[i+1:]
+		}
+	}
+	return pos2d{x, y, currentKeys}
+}
+
+type pos2d struct {
+	X, Y int
+	Keys string
+}
+
+func (p pos2d) Final(b dijkstra.Board) bool {
+	bb, ok := b.(tractorMaze)
+	if !ok {
+		return false
+	}
+
+	return p.Keys == bb.KeyRequirement
+}
+func (p pos2d) Adjacent(b dijkstra.Board) dijkstra.AdjacencyIterator {
+	bb, ok := b.(tractorMaze)
+	if !ok {
+		return nil
+	}
+
+	return &pos2diter{
+		pos: p,
+		b:   bb,
+		idx: 0,
+	}
+}
+
+type pos2diter struct {
+	pos pos2d
+	b   tractorMaze
+	idx int
+}
+
+func (pdi *pos2diter) Next() (dijkstra.Position, int) {
+	pdi.idx++
+	found := 0
+
+	rv := pdi.b.posAt(pdi.pos.X+1, pdi.pos.Y, pdi.pos.Keys)
+	if pdi.isOk(rv) {
+		found++
+		if found == pdi.idx {
+			return rv, 1
+		}
+	}
+
+	rv = pdi.b.posAt(pdi.pos.X, pdi.pos.Y+1, pdi.pos.Keys)
+	if pdi.isOk(rv) {
+		found++
+		if found == pdi.idx {
+			return rv, 1
+		}
+	}
+
+	rv = pdi.b.posAt(pdi.pos.X-1, pdi.pos.Y, pdi.pos.Keys)
+	if pdi.isOk(rv) {
+		found++
+		if found == pdi.idx {
+			return rv, 1
+		}
+	}
+
+	rv = pdi.b.posAt(pdi.pos.X, pdi.pos.Y-1, pdi.pos.Keys)
+	if pdi.isOk(rv) {
+		found++
+		if found == pdi.idx {
+			return rv, 1
+		}
+	}
+
+	return nil, 0
+}
+
+func (pdi *pos2diter) isOk(p pos2d) bool {
+	c := pdi.b.charAt(p.X, p.Y)
+	if c == '#' {
+		return false
+	}
+
+	if c >= 'A' && c <= 'Z' {
+		return p.Keys[int(c-'A')] == byte(c+' ')
+	}
+
+	return true
+}
+
 // func Dec18b(ctx ch.AOContext) error {
 // 	return errNotImplemented
 // }
-
---
+++ lib/dijkstra/dijkstra.go
@@ -0,0 +1,117 @@
+package dijkstra
+
+import (
+	"errors"
+)
+
+type Board interface {
+	// StartingPositions returns all possible starting positions from which to find the shortest path
+	StartingPositions() []Position
+}
+
+type Position interface {
+	Final(b Board) bool
+	Adjacent(b Board) AdjacencyIterator
+}
+
+type AdjacencyIterator interface {
+	Next() (Position, int)
+}
+
+func ShortestPath(b Board) ([]Position, int, error) {
+	starts := b.StartingPositions()
+
+	dijk := &dijkstra{
+		Board:   b,
+		Visited: make(map[Position]dijkHead),
+	}
+	for _, pos := range starts {
+		dijk.Heads = append(dijk.Heads, dijkHead{
+			Position:  pos,
+			TotalCost: 0,
+		})
+	}
+
+	for i := 1; i < 10000; i++ {
+		dijk.Step()
+		//log.Printf("Step %d; heads: %v", i, dijk.Heads)
+		if len(dijk.Heads) == 0 {
+			break
+		}
+	}
+
+	if dijk.Shortest.Position == nil {
+		return nil, 0, errors.New("Failed to find the shortest path")
+	}
+
+	// TODO: return list of steps
+
+	return nil, dijk.Shortest.TotalCost, nil
+}
+
+type dijkHead struct {
+	Position  Position
+	TotalCost int
+}
+
+type dijkstra struct {
+	Board    Board
+	Heads    []dijkHead
+	Shortest dijkHead
+	Visited  map[Position]dijkHead
+}
+
+func (d *dijkstra) Step() {
+	newHeads := []dijkHead{}
+
+	for i, h := range d.Heads {
+		it := h.Position.Adjacent(d.Board)
+		first := true
+		for {
+			n, cost := it.Next()
+			if n == nil {
+				break
+			}
+
+			newCost := h.TotalCost + cost
+			if d.Shortest.Position != nil && d.Shortest.TotalCost < newCost {
+				// The current partial path is already longer than the shortest path that finishes
+				continue
+			}
+
+			val, ok := d.Visited[n]
+			if !ok || val.TotalCost > newCost {
+				val = dijkHead{
+					Position:  h.Position,
+					TotalCost: newCost,
+				}
+				d.Visited[n] = val
+
+				if first {
+					d.Heads[i].Position = n
+					d.Heads[i].TotalCost = newCost
+					first = false
+				} else {
+					newHeads = append(newHeads, dijkHead{n, newCost})
+				}
+			}
+
+			if n.Final(d.Board) {
+				if d.Shortest.Position == nil || d.Shortest.TotalCost < newCost {
+					d.Shortest.Position = n
+					d.Shortest.TotalCost = newCost
+				}
+			}
+		}
+		if first {
+			d.Heads[i].Position = nil
+		}
+	}
+
+	for i := len(d.Heads) - 1; i >= 0; i-- {
+		if d.Heads[i].Position == nil {
+			d.Heads = append(d.Heads[:i], d.Heads[i+1:]...)
+		}
+	}
+	d.Heads = append(d.Heads, newHeads...)
+}
