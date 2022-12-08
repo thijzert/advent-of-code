eb96bc0c73af151af05cfda5b0e1343e0e2765bd
Thijs van Dijk <github@inurbanus.nl>
2022-12-08 19:32:25 +0100

    Scale the Walking into higher dimensions

--- lib/cube/direction.go
+++ lib/cube/direction.go
@@ -1,7 +1,10 @@
 package cube
 
-// Cardinal2D contains al cardinal directions in 2D space (right, up, left, down)
+// Cardinal2D contains all cardinal directions in 2D space (right, up, left, down)
 var Cardinal2D [4]Point = [4]Point{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}
+
+// Cardinal2Diag contains all unit directions in 2D space, including diagonals
+var Cardinal2Diag [8]Point = [8]Point{{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}}
 
 // A Walker represents a callback function to be passed to Walk(). If the
 // Walker returns true, the Walk is aborted before reaching the boundaries
@@ -12,6 +15,50 @@ type Walker func(p Point) bool
 // Walk returns the number of steps taken, and a bool indicating if the walk
 // was aborted prematurely or ran outside of the given boundaries.
 func Walk(start, direction Point, bounds Square, f Walker) (int, bool) {
+	p := start
+	rv := 0
+	for bounds.Contains(p) {
+		if f(p) {
+			return rv, true
+		}
+		p = p.Add(direction)
+		rv++
+	}
+	rv--
+	return rv, false
+}
+
+// A Flyer represent a callback function to be passed to Fly().
+// Flyer is like a Walker, but in 3D space
+type Flyer func(p Point3) bool
+
+// Fly moves in the given direction from the start point until you exit the
+// boundaries, calling f at every step. If f returns true, the flight is
+// aborted. Fly returns the number of steps taken, and a bool indicating if
+// the flight was aborted prematurely or ran outside of the given boundaries.
+func Fly(start, direction Point3, bounds Cube, f Flyer) (int, bool) {
+	p := start
+	rv := 0
+	for bounds.Contains(p) {
+		if f(p) {
+			return rv, true
+		}
+		p = p.Add(direction)
+		rv++
+	}
+	rv--
+	return rv, false
+}
+
+// A Phaser represent a callback function to be passed to Fly().
+// Phaser is like a Walker, but in 4D space
+type Phaser func(p Point4) bool
+
+// Phase moves in the given direction from the start point until you exit the
+// boundaries, calling f at every step. If f returns true, the phase is
+// aborted. Phase returns the number of steps taken, and a bool indicating if
+// the phase was aborted prematurely or ran outside of the given boundaries.
+func Phase(start, direction Point4, bounds Hypercube, f Phaser) (int, bool) {
 	p := start
 	rv := 0
 	for bounds.Contains(p) {
