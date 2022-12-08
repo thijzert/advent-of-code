69a6c62676a3ef40b2bb9b1713ac6c42011e57ac
Thijs van Dijk <github@inurbanus.nl>
2022-12-08 07:37:43 +0100

    Solve 2022-08-A

--- ch/aoc22/dec08.go
+++ ch/aoc22/dec08.go
@@ -4,20 +4,48 @@ import (
 	"github.com/thijzert/advent-of-code/ch"
 )
 
-var Dec08a ch.AdventFunc = nil
+func Dec08a(ctx ch.AOContext) error {
+	forest, err := ctx.DataLines("inputs/2022/dec08.txt")
+	if err != nil {
+		return err
+	}
+	//forest = []string{"30373", "25512", "65332", "33549", "35390"}
+
+	visibleTrees := 0
+	for y := range forest {
+		for x, height := range forest[y] {
+			visTop, visBottom, visLeft, visRight := true, true, true, true
+			for i, h := range forest[y] {
+				if h >= height {
+					if i > x {
+						visRight = false
+					} else if i < x {
+						visLeft = false
+					}
+				}
+			}
+			for i := range forest {
+				h := rune(forest[i][x])
+				if h >= height && i != y {
+					if i > y {
+						visBottom = false
+					} else if i < y {
+						visTop = false
+					}
+				}
+			}
+			if visTop || visBottom || visLeft || visRight {
+				visibleTrees++
+			}
+		}
+	}
+
+	ctx.FinalAnswer.Print(visibleTrees)
+	return errNotImplemented
+}
+
 var Dec08b ch.AdventFunc = nil
 
-// func Dec08a(ctx ch.AOContext) error {
-// 	sections, err := ctx.DataSections("inputs/2022/dec08a.txt")
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	ctx.Print(len(sections))
-// 	return errNotImplemented
-// }
-//
 // func Dec08b(ctx ch.AOContext) error {
 // 	return errNotImplemented
 // }
-
