44b53d36186808b5f55653900ba6f980d5747d78
Thijs van Dijk <github@inurbanus.nl>
2022-12-10 12:07:41 +0100

    Solve 2022-10

--- ch/aoc22/dec10.go
+++ ch/aoc22/dec10.go
@@ -1,23 +1,85 @@
 package aoc22
 
 import (
+	"fmt"
+
 	"github.com/thijzert/advent-of-code/ch"
 )
 
-var Dec10a ch.AdventFunc = nil
-var Dec10b ch.AdventFunc = nil
+func Dec10a(ctx ch.AOContext) error {
+	program, err := ctx.DataLines("inputs/2022/dec10.txt")
+	if err != nil {
+		return err
+	}
 
-// func Dec10a(ctx ch.AOContext) error {
-// 	sections, err := ctx.DataSections("inputs/2022/dec10a.txt")
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	ctx.Print(len(sections))
-// 	return errNotImplemented
-// }
-//
-// func Dec10b(ctx ch.AOContext) error {
-// 	return errNotImplemented
-// }
+	signalStrength := 0
+	x := 1
+	cycleCount := 0
+	tick := func() {
+		cycleCount++
+		if (cycleCount+20)%40 == 0 {
+			ctx.Printf("During the %dth cycle, register X has the value %d, so the signal strength is %d * %d = %d.", cycleCount, x, cycleCount, x, cycleCount*x)
+			signalStrength += cycleCount * x
+		}
+	}
 
+	for _, instr := range program {
+		if instr == "noop" {
+			tick()
+		} else if len(instr) > 5 && instr[:5] == "addx " {
+			var operand int
+			_, err := fmt.Sscanf(instr, "addx %d", &operand)
+			if err != nil {
+				return err
+			}
+			tick()
+			tick()
+			x += operand
+		} else {
+			return fmt.Errorf("unknown instruction '%s'", instr)
+		}
+	}
+
+	ctx.FinalAnswer.Print(signalStrength)
+	return nil
+}
+
+func Dec10b(ctx ch.AOContext) error {
+	program, err := ctx.DataLines("inputs/2022/dec10.txt")
+	if err != nil {
+		return err
+	}
+
+	x := 1
+	cycleCount := 0
+	scanline := make([]byte, 40)
+	tick := func() {
+		i := cycleCount % 40
+		cycleCount++
+		if x >= i-1 && x <= i+1 {
+			scanline[i] = '#'
+		} else {
+			scanline[i] = ' '
+		}
+		if i == 39 {
+			ctx.Printf("%s", scanline)
+		}
+	}
+
+	for _, instr := range program {
+		if instr == "noop" {
+			tick()
+		} else if len(instr) > 5 && instr[:5] == "addx " {
+			var operand int
+			fmt.Sscanf(instr, "addx %d", &operand) // We checked the validity in the last challenge
+			tick()
+			tick()
+			x += operand
+		} else {
+			return fmt.Errorf("unknown instruction '%s'", instr)
+		}
+	}
+
+	ctx.FinalAnswer.Print("(Maybe I should implement OCR at some point)")
+	return nil
+}
