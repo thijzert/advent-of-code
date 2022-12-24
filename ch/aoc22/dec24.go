1dea8abbace5df2773683c2c2a462cdd552a9f9e
Thijs van Dijk <github@inurbanus.nl>
2022-12-24 11:58:38 +0100

    Solve 2022-24-B

--- ch/aoc22/dec24.go
+++ ch/aoc22/dec24.go
@@ -21,16 +21,29 @@ func Dec24a(ctx ch.AOContext) (interface{}, error) {
 	return cost, nil
 }
 
-var Dec24b ch.AdventFunc = nil
+func Dec24b(ctx ch.AOContext) (interface{}, error) {
+	snowstorm, err := readSnowstormboard(ctx, "inputs/2022/dec24.txt")
+	if err != nil {
+		return nil, err
+	}
 
-// func Dec24b(ctx ch.AOContext) (interface{}, error) {
-// 	return nil, errNotImplemented
-// }
+	snowstorm.SnackRequirement = 2
 
-type snowstormboard [4]*image.Image
+	_, cost, err := dijkstra.ShortestPath(snowstorm)
+	if err != nil {
+		return nil, err
+	}
 
-func (snowstormboard) StartingPositions() []dijkstra.Position {
-	return []dijkstra.Position{expeditionPosition{0, -1, 0}}
+	return cost, nil
+}
+
+type snowstormboard struct {
+	Storms           [4]*image.Image
+	SnackRequirement int
+}
+
+func (b snowstormboard) StartingPositions() []dijkstra.Position {
+	return []dijkstra.Position{expeditionPosition{0, -1, 0, b.SnackRequirement}}
 }
 
 func readSnowstormboard(ctx ch.AOContext, name string) (snowstormboard, error) {
@@ -57,23 +70,23 @@ func readSnowstormboard(ctx ch.AOContext, name string) (snowstormboard, error) {
 		}
 	}
 
-	rv[0] = image.ReadImage(crustsRemoved, filterRune('<'))
-	rv[1] = image.ReadImage(crustsRemoved, filterRune('>'))
-	rv[2] = image.ReadImage(crustsRemoved, filterRune('^'))
-	rv[3] = image.ReadImage(crustsRemoved, filterRune('v'))
+	rv.Storms[0] = image.ReadImage(crustsRemoved, filterRune('<'))
+	rv.Storms[1] = image.ReadImage(crustsRemoved, filterRune('>'))
+	rv.Storms[2] = image.ReadImage(crustsRemoved, filterRune('^'))
+	rv.Storms[3] = image.ReadImage(crustsRemoved, filterRune('v'))
 
-	ctx.Printf("Snowstorms going left:\n%s\n, going down:\n%s", rv[0], rv[3])
+	//ctx.Printf("Snowstorms going left:\n%s\n, going down:\n%s", rv.Storms[0], rv.Storms[3])
 	return rv, nil
 }
 
-type expeditionPosition cube.Point3
+type expeditionPosition cube.Point4
 
 func (p expeditionPosition) Final(b dijkstra.Board) bool {
 	bb, ok := b.(snowstormboard)
 	if !ok {
 		return false
 	}
-	return p.X == bb[0].Width-1 && p.Y == bb[0].Height
+	return p.X == bb.Storms[0].Width-1 && p.Y == bb.Storms[0].Height && p.W == 0
 }
 
 func (p expeditionPosition) Adjacent(b dijkstra.Board, totalCost int) dijkstra.AdjacencyIterator {
@@ -82,12 +95,21 @@ func (p expeditionPosition) Adjacent(b dijkstra.Board, totalCost int) dijkstra.A
 		return dijkstra.DeadEnd()
 	}
 
-	W, H := bb[0].Width, bb[0].Height
+	W, H := bb.Storms[0].Width, bb.Storms[0].Height
 
 	rv := []dijkstra.Adj{}
-	for _, add := range [5]cube.Point3{{0, 0, 1}, {1, 0, 1}, {0, 1, 1}, {-1, 0, 1}, {0, -1, 1}} {
-		pt := expeditionPosition(cube.Point3(p).Add(add))
-		if (pt.X == W-1 && pt.Y == H) || (pt.X == 0 && pt.Y == -1) {
+	for _, add := range [5]cube.Point4{{0, 0, 1, 0}, {1, 0, 1, 0}, {0, 1, 1, 0}, {-1, 0, 1, 0}, {0, -1, 1, 0}} {
+		pt := expeditionPosition(cube.Point4(p).Add(add))
+		if pt.X == W-1 && pt.Y == H {
+			if pt.W > 0 && pt.W%2 == 0 {
+				pt.W--
+			}
+			rv = append(rv, dijkstra.Adj{pt, 1})
+			continue
+		} else if pt.X == 0 && pt.Y == -1 {
+			if pt.W%2 == 1 {
+				pt.W--
+			}
 			rv = append(rv, dijkstra.Adj{pt, 1})
 			continue
 		} else if pt.X < 0 || pt.X >= W || pt.Y < 0 || pt.Y >= H {
@@ -95,10 +117,10 @@ func (p expeditionPosition) Adjacent(b dijkstra.Board, totalCost int) dijkstra.A
 		}
 		storms := 0
 		t := totalCost + 1
-		storms += bb[0].At((pt.X+t)%W, pt.Y)
-		storms += bb[1].At(((pt.X-t)%W+W)%W, pt.Y)
-		storms += bb[2].At(pt.X, (pt.Y+t)%H)
-		storms += bb[3].At(pt.X, ((pt.Y-t)%H+H)%H)
+		storms += bb.Storms[0].At((pt.X+t)%W, pt.Y)
+		storms += bb.Storms[1].At(((pt.X-t)%W+W)%W, pt.Y)
+		storms += bb.Storms[2].At(pt.X, (pt.Y+t)%H)
+		storms += bb.Storms[3].At(pt.X, ((pt.Y-t)%H+H)%H)
 		if storms == 0 {
 			rv = append(rv, dijkstra.Adj{pt, 1})
 		}
