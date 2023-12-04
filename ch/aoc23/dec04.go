9b06a5246b444e968585d3adf62becda85f17a38
Thijs van Dijk <github@inurbanus.nl>
2023-12-04 06:41:57 +0100

    Solve 2023-04

--- ch/aoc23/dec04.go
+++ ch/aoc23/dec04.go
@@ -1,22 +1,82 @@
 package aoc23
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/thijzert/advent-of-code/ch"
 )
 
-var Dec04a ch.AdventFunc = nil
-// func Dec04a(ctx ch.AOContext) (interface{}, error) {
-// 	sections, err := ctx.DataSections("inputs/2023/dec04a.txt")
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	ctx.Print(len(sections))
-// 	return nil, errNotImplemented
-// }
+func atoid(s string, def int) int {
+	rv, err := strconv.Atoi(s)
+	if err != nil {
+		return def
+	}
+	return rv
+}
 
-var Dec04b ch.AdventFunc = nil
-// func Dec04b(ctx ch.AOContext) (interface{}, error) {
-// 	return nil, errNotImplemented
-// }
+func Dec04a(ctx ch.AOContext) (interface{}, error) {
+	lines, err := ctx.DataLines("inputs/2023/dec04.txt")
+	if err != nil {
+		return nil, err
+	}
 
+	winnings := 0
+	for i, line := range lines {
+		contents := strings.Split(strings.Split(line, ":")[1], "|")
+		winning := make(map[int]bool)
+		for _, s := range strings.Split(contents[0], " ") {
+			if s != "" {
+				winning[atoid(s, -1)] = true
+			}
+		}
+
+		worth := 1
+		for _, s := range strings.Split(contents[1], " ") {
+			if s != "" && winning[atoid(s, 0)] {
+				worth <<= 1
+			}
+		}
+		ctx.Printf("Card %d is worth %d points", i+1, worth>>1)
+		winnings += worth >> 1
+	}
+
+	return winnings, nil
+}
+
+func Dec04b(ctx ch.AOContext) (interface{}, error) {
+	lines, err := ctx.DataLines("inputs/2023/dec04.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	multipliers := make([]int, len(lines))
+	for i := range multipliers {
+		multipliers[i] = 1
+	}
+
+	totalCards := 0
+	for i, line := range lines {
+		contents := strings.Split(strings.Split(line, ":")[1], "|")
+		winning := make(map[int]bool)
+		for _, s := range strings.Split(contents[0], " ") {
+			if s != "" {
+				winning[atoid(s, -1)] = true
+			}
+		}
+
+		matching := 0
+		for _, s := range strings.Split(contents[1], " ") {
+			if s != "" && winning[atoid(s, 0)] {
+				matching++
+			}
+		}
+		ctx.Printf("Your %d instances of card %d have %d matching numbers", multipliers[i], i+1, matching)
+		for j := 0; j < matching; j++ {
+			multipliers[i+j+1] += multipliers[i]
+		}
+		totalCards += multipliers[i]
+	}
+
+	return totalCards, nil
+}
