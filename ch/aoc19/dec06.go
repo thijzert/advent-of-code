94dbb0fffc814ce31da42b8395d4254d1d45ee54
Thijs van Dijk <github@inurbanus.nl>
2023-12-07 08:48:34 +0100

    Solve 2019-6-A

--- ch/aoc19/dec06.go
+++ ch/aoc19/dec06.go
@@ -1,22 +1,46 @@
 package aoc19
 
 import (
+	"strings"
+
 	"github.com/thijzert/advent-of-code/ch"
 )
 
-var Dec06a ch.AdventFunc = nil
+func Dec06a(ctx ch.AOContext) (interface{}, error) {
+	lines, err := ctx.DataLines("inputs/2019/dec06.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	parent := make(map[string]string)
+	for _, line := range lines {
+		lpts := strings.Split(line, ")")
+		parent[lpts[1]] = lpts[0]
+	}
+
+	totalIndirectOrbits := 0
+	level := make(map[string]int)
+	level["COM"] = 0
+	done := false
+	for !done {
+		done = true
+		for k, v := range parent {
+			if _, ok := level[k]; !ok {
+				done = false
+				if l, ok := level[v]; ok {
+					level[k] = l + 1
+					totalIndirectOrbits += l + 1
+				}
+			}
+		}
+	}
+	ctx.Printf("Levels: %v", level)
+
+	return totalIndirectOrbits, nil
+}
+
 var Dec06b ch.AdventFunc = nil
 
-// func Dec06a(ctx ch.AOContext) (interface{}, error) {
-// 	sections, err := ctx.DataSections("inputs/2019/dec06a.txt")
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	ctx.Print(len(sections))
-// 	return nil, errNotImplemented
-// }
-//
 // func Dec06b(ctx ch.AOContext) (interface{}, error) {
 // 	return nil, errNotImplemented
 // }
