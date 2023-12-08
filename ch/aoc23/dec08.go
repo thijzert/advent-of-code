2d27bbeef83876883556d0148210eff3186b6e26
Thijs van Dijk <github@inurbanus.nl>
2023-12-08 07:43:21 +0100

    Solve 2023-08

--- ch/aoc23/dec08.go
+++ ch/aoc23/dec08.go
@@ -4,19 +4,90 @@ import (
 	"github.com/thijzert/advent-of-code/ch"
 )
 
-var Dec08a ch.AdventFunc = nil
-// func Dec08a(ctx ch.AOContext) (interface{}, error) {
-// 	sections, err := ctx.DataSections("inputs/2023/dec08a.txt")
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	ctx.Print(len(sections))
-// 	return nil, errNotImplemented
-// }
+func Dec08a(ctx ch.AOContext) (interface{}, error) {
+	lines, err := ctx.DataLines("inputs/2023/dec08.txt")
+	if err != nil {
+		return nil, err
+	}
 
-var Dec08b ch.AdventFunc = nil
-// func Dec08b(ctx ch.AOContext) (interface{}, error) {
-// 	return nil, errNotImplemented
-// }
+	pattern := lines[0]
+	nodes := make(map[string][2]string)
+	for _, line := range lines[2:] {
+		nodes[line[:3]] = [2]string{line[7:10], line[12:15]}
+	}
 
+	i := 0
+	n := "AAA"
+	for n != "ZZZ" {
+		c := pattern[i%len(pattern)]
+		i++
+		if c == 'L' {
+			n = nodes[n][0]
+		} else {
+			n = nodes[n][1]
+		}
+	}
+
+	return i, nil
+}
+
+func Dec08b(ctx ch.AOContext) (interface{}, error) {
+	lines, err := ctx.DataLines("inputs/2023/dec08.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	pattern := lines[0]
+	nodes := make(map[string][2]string)
+	for _, line := range lines[2:] {
+		nodes[line[:3]] = [2]string{line[7:10], line[12:15]}
+	}
+
+	answer := 1
+
+	for k := range nodes {
+		if k[2] == 'A' {
+			n := k
+			i, offset, modulus := 0, 0, 0
+			first := ""
+			for n != first {
+				if n[2] == 'Z' {
+					if first == "" {
+						first = n
+						offset = i
+					}
+				}
+
+				c := pattern[i%len(pattern)]
+				i++
+				if c == 'L' {
+					n = nodes[n][0]
+				} else {
+					n = nodes[n][1]
+				}
+			}
+			modulus = i - offset
+			ctx.Printf("Ghost %s finishes in %d + k·%d steps", k, offset, modulus)
+			if offset%modulus != 0 {
+				return nil, errFailed
+			}
+
+			answer = lcm(answer, modulus)
+		}
+	}
+
+	return answer, errNotImplemented
+}
+
+func lcm(a, b int) int {
+	return a * b / gcd(a, b)
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
