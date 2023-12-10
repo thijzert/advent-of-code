b281f0f6afe563edffc6dbea968b5514102319f6
Thijs van Dijk <github@inurbanus.nl>
2023-12-10 21:21:50 +0100

    Prefer named opcodes over magic integer values

--- ch/aoc19/intcode.go
+++ ch/aoc19/intcode.go
@@ -5,6 +5,19 @@ import (
 	"sync"
 )
 
+const (
+	OP_ADD    int = 1
+	OP_MULT       = 2
+	OP_READ       = 3
+	OP_WRITE      = 4
+	OP_JTRUE      = 5 // Jump if True
+	OP_JFALSE     = 6 // Jump if False
+	OP_LESS       = 7
+	OP_EQUALS     = 8
+	OP_CHRB       = 9 // Change Relative Base
+	OP_HALT       = 99
+)
+
 func runIntCodeProgram(program []int, input []int, output []int) (int, int, int, error) {
 	chin, chout := make(chan int), make(chan int)
 	inputPtr, outputPtr := 0, 0
@@ -56,42 +69,42 @@ func startIntCodeProgram(program []int, input chan int, output chan int) (int, e
 	}
 
 	pc := 0
-	for pc >= 0 && pc <= len(memory) && memory[pc] != 99 {
+	for pc >= 0 && pc <= len(memory) && memory[pc] != OP_HALT {
 		opcode := memory[pc] % 100
 		modeA, modeB, modeC := (memory[pc]/100)%10, (memory[pc]/1000)%10, (memory[pc]/10000)%10
 
-		if opcode == 1 {
+		if opcode == OP_ADD {
 			set(pc+3, modeC, get(pc+1, modeA)+get(pc+2, modeB))
 			pc += 4
-		} else if opcode == 2 {
+		} else if opcode == OP_MULT {
 			set(pc+3, modeC, get(pc+1, modeA)*get(pc+2, modeB))
 			pc += 4
-		} else if opcode == 3 {
-			set(pc+1, modeB, <-input)
+		} else if opcode == OP_READ {
+			set(pc+1, modeA, <-input)
 			pc += 2
-		} else if opcode == 4 {
-			output <- get(pc+1, modeB)
+		} else if opcode == OP_WRITE {
+			output <- get(pc+1, modeA)
 			pc += 2
-		} else if opcode == 5 {
+		} else if opcode == OP_JTRUE {
 			if get(pc+1, modeA) != 0 {
 				pc = get(pc+2, modeB)
 			} else {
 				pc += 3
 			}
-		} else if opcode == 6 {
+		} else if opcode == OP_JFALSE {
 			if get(pc+1, modeA) == 0 {
 				pc = get(pc+2, modeB)
 			} else {
 				pc += 3
 			}
-		} else if opcode == 7 {
+		} else if opcode == OP_LESS {
 			v := 0
 			if get(pc+1, modeA) < get(pc+2, modeB) {
 				v = 1
 			}
 			set(pc+3, modeC, v)
 			pc += 4
-		} else if opcode == 8 {
+		} else if opcode == OP_EQUALS {
 			v := 0
 			if get(pc+1, modeA) == get(pc+2, modeB) {
 				v = 1
