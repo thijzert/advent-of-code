411b6ab2a614eb37d39010cc9f291ee2739e72d2
Thijs van Dijk <github@inurbanus.nl>
2023-12-12 07:24:46 +0100

    Solve 2023-12... poorly

--- ch/aoc19/dec10.go
+++ ch/aoc19/dec10.go
@@ -2,21 +2,60 @@ package aoc19
 
 import (
 	"github.com/thijzert/advent-of-code/ch"
+	"github.com/thijzert/advent-of-code/lib/image"
 )
 
-var Dec10a ch.AdventFunc = nil
+func Dec10a(ctx ch.AOContext) (interface{}, error) {
+	lines, err := ctx.DataLines("inputs/2019/dec10.txt")
+	if err != nil {
+		return nil, err
+	}
+	lines = []string{"......#.#.", "#..#.#....", "..#######.", ".#.#.###..", ".#..#.....", "..#....#.#", "#..#....#.", ".##.#..###", "##...#..#.", ".#....####"}
+	img := image.ReadImage(lines, image.Octothorpe)
+
+	ctx.Printf("Asteroid belt:\n%s", img)
+	bestX, bestY, bestAst := 0, 0, 0
+	for y := range lines {
+		for x := range lines[0] {
+			for b := range lines {
+				for a := range lines[0] {
+					dx, dy := a-x, b-y
+					if (dx == 0 && dy == 0) || img.At(a, b) == 0 {
+						continue
+					}
+					for c := range lines[2:] {
+						aa, bb := x+(c+2)*dx, y+(c+2)*dy
+						img.Set(aa, bb, -img.At(aa, bb))
+					}
+				}
+			}
+			ast := 0
+			for b := range lines {
+				for a := range lines[0] {
+					if a == x && b == y {
+						continue
+					}
+					v := img.At(a, b)
+					if v == -1 {
+						img.Set(a, b, 1)
+					} else {
+						ast += v
+					}
+				}
+			}
+			if ast > bestAst {
+				bestX, bestY, bestAst = x, y, ast
+			}
+		}
+	}
+
+	ctx.Printf("Best location is %d,%d with %d asteroids detected", bestX, bestY, bestAst)
+
+	return bestAst, nil
+}
+
 var Dec10b ch.AdventFunc = nil
 
-// func Dec10a(ctx ch.AOContext) (interface{}, error) {
-// 	sections, err := ctx.DataSections("inputs/2019/dec10a.txt")
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	ctx.Print(len(sections))
-// 	return nil, errNotImplemented
-// }
-//
 // func Dec10b(ctx ch.AOContext) (interface{}, error) {
 // 	return nil, errNotImplemented
 // }
--- ch/aoc23/dec12.go
+++ ch/aoc23/dec12.go
@@ -1,22 +1,98 @@
 package aoc23
 
 import (
+	"strings"
+
 	"github.com/thijzert/advent-of-code/ch"
+	"github.com/thijzert/advent-of-code/data"
 )
 
-var Dec12a ch.AdventFunc = nil
-// func Dec12a(ctx ch.AOContext) (interface{}, error) {
-// 	sections, err := ctx.DataSections("inputs/2023/dec12a.txt")
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	ctx.Print(len(sections))
-// 	return nil, errNotImplemented
-// }
+func dec12Arrangements(ctx ch.AOContext, conditionRecord []byte, checksum []int) int {
+	if len(checksum) == 0 {
+		for _, b := range conditionRecord {
+			if b == '#' {
+				return 0
+			}
+		}
+		return 1
+	}
 
-var Dec12b ch.AdventFunc = nil
-// func Dec12b(ctx ch.AOContext) (interface{}, error) {
-// 	return nil, errNotImplemented
-// }
+	n := checksum[0]
+	//ctx.Printf("  try and fit a %d in %s", n, conditionRecord)
 
+	rv := 0
+	for i := range conditionRecord {
+		// Suppose we put checksum[0] at index i
+
+		if i > 0 && conditionRecord[i-1] == '#' {
+			// nvm, they've already started.
+			break
+		}
+
+		// Check if we can place n octothorpes at all
+		if len(conditionRecord[i:]) < n {
+			continue
+		}
+		canPlace := true
+		for j := 0; j < n; j++ {
+			canPlace = canPlace && conditionRecord[i+j] != '.'
+		}
+		if !canPlace {
+			continue
+		}
+		if len(conditionRecord[i:]) == n {
+			// We are at the end of the record. This is either good or bad news
+			if len(checksum) == 1 {
+				rv += 1
+			}
+			continue
+		}
+		if conditionRecord[i+n] == '#' {
+			continue
+		}
+		rv += dec12Arrangements(ctx, conditionRecord[i+n+1:], checksum[1:])
+	}
+	//ctx.Printf("   %d ways", rv)
+
+	return rv
+}
+
+func Dec12a(ctx ch.AOContext) (interface{}, error) {
+	lines, err := ctx.DataLines("inputs/2023/dec12.txt")
+	if err != nil {
+		return nil, err
+	}
+	//lines = []string{"???.### 1,1,3", ".??..??...?##. 1,1,3", "?#?#?#?#?#?#?#? 1,3,1,6", "????.#...#... 4,1,1", "????.######..#####. 1,6,5", "?###???????? 3,2,1"}
+
+	answer := 0
+	for _, line := range lines {
+		parts := strings.Split(line, " ")
+		arr := dec12Arrangements(ctx, []byte(parts[0]), data.CSVInts(parts[1:])[0])
+		ctx.Printf("%s - %d arrangements", line, arr)
+		answer += arr
+	}
+
+	// 7760: too low
+	return answer, nil
+}
+
+func Dec12b(ctx ch.AOContext) (interface{}, error) {
+	lines, err := ctx.DataLines("inputs/2023/dec12.txt")
+	if err != nil {
+		return nil, err
+	}
+	//lines = []string{"???.### 1,1,3", ".??..??...?##. 1,1,3", "?#?#?#?#?#?#?#? 1,3,1,6", "????.#...#... 4,1,1", "????.######..#####. 1,6,5", "?###???????? 3,2,1"}
+
+	answer := 0
+	for _, line := range lines {
+		parts := strings.Split(line, " ")
+		parts[0] = strings.Repeat("?"+parts[0], 5)[1:]
+		parts[1] = strings.Repeat(","+parts[1], 5)[1:]
+		arr := dec12Arrangements(ctx, []byte(parts[0]), data.CSVInts(parts[1:])[0])
+		ctx.Printf("%s - %d arrangements", line, arr)
+		answer += arr
+	}
+
+	// 7760: too low
+	return answer, nil
+}
