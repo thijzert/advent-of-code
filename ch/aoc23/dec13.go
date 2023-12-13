c3474d237c7ee6a8f7dffc8cc30d41eb7cac2e86
Thijs van Dijk <github@inurbanus.nl>
2023-12-13 09:02:00 +0100

    Solve 2023-13-B

--- ch/aoc23/dec13.go
+++ ch/aoc23/dec13.go
@@ -6,6 +6,14 @@ import (
 )
 
 func Dec13a(ctx ch.AOContext) (interface{}, error) {
+	return dec13(ctx, 0)
+}
+
+func Dec13b(ctx ch.AOContext) (interface{}, error) {
+	return dec13(ctx, 1)
+}
+
+func dec13(ctx ch.AOContext, smudge int) (interface{}, error) {
 	sections, err := ctx.DataSections("inputs/2023/dec13.txt")
 	if err != nil {
 		return nil, err
@@ -14,10 +22,10 @@ func Dec13a(ctx ch.AOContext) (interface{}, error) {
 	answer := 0
 	err = nil
 	for i, sect := range sections {
-		if j, ok := dec13findAxis(sect); ok {
+		if j, ok := dec13findAxis(sect, smudge); ok {
 			ctx.Printf("section %d reflects in the %dth row", i+1, j)
 			answer += 100 * j
-		} else if j, ok := dec13findAxis(data.Transpose(sect)); ok {
+		} else if j, ok := dec13findAxis(data.Transpose(sect), smudge); ok {
 			ctx.Printf("section %d reflects in the %dth column", i+1, j)
 			answer += j
 		} else {
@@ -29,27 +37,32 @@ func Dec13a(ctx ch.AOContext) (interface{}, error) {
 	return answer, err
 }
 
-var Dec13b ch.AdventFunc = nil
-
-// func Dec13b(ctx ch.AOContext) (interface{}, error) {
-// 	return nil, errNotImplemented
-// }
-
-func dec13findAxis(lines []string) (int, bool) {
+func dec13findAxis(lines []string, smudgeFactor int) (int, bool) {
 	for i := range lines {
 		if i == len(lines)-1 {
 			continue
 		}
 		l := min(i, len(lines)-i-2)
 
-		ok := true
+		dist := 0
 		for j := 0; j <= l; j++ {
-			ok = ok && lines[i-j] == lines[i+j+1]
+			dist += dec13stringdist(lines[i-j], lines[i+j+1])
 		}
-		if ok {
+		if dist == smudgeFactor {
 			return i + 1, true
 		}
 	}
 
 	return 0, false
+}
+
+func dec13stringdist(a, b string) int {
+	rv := 0
+	l := len(a)
+	for i := 0; i < l; i++ {
+		if a[i] != b[i] {
+			rv++
+		}
+	}
+	return rv
 }
