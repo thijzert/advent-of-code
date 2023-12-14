f4bc6b3d737a7c29e756a985b79e50bc10170dea
Thijs van Dijk <github@inurbanus.nl>
2023-12-14 07:03:11 +0100

    Solve 2023-14-A

--- ch/aoc23/dec14.go
+++ ch/aoc23/dec14.go
@@ -2,21 +2,55 @@ package aoc23
 
 import (
 	"github.com/thijzert/advent-of-code/ch"
+	"github.com/thijzert/advent-of-code/lib/image"
 )
 
-var Dec14a ch.AdventFunc = nil
-// func Dec14a(ctx ch.AOContext) (interface{}, error) {
-// 	sections, err := ctx.DataSections("inputs/2023/dec14a.txt")
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	ctx.Print(len(sections))
-// 	return nil, errNotImplemented
-// }
+func Dec14a(ctx ch.AOContext) (interface{}, error) {
+	lines, err := ctx.DataLines("inputs/2023/dec14.txt")
+	if err != nil {
+		return nil, err
+	}
+	img := image.ReadImage(lines, dec14rocks)
+
+	ctx.Printf("Rocks:\n%s", img)
+	answer := dec14RollNorth(ctx, img)
+	ctx.Printf("After rolling:\n%s", img)
+
+	return answer, nil
+}
 
 var Dec14b ch.AdventFunc = nil
+
 // func Dec14b(ctx ch.AOContext) (interface{}, error) {
 // 	return nil, errNotImplemented
 // }
 
+func dec14RollNorth(ctx ch.AOContext, img *image.Image) int {
+	rv := 0
+	for x := 0; x < img.Width; x++ {
+		next := 0
+		for y := 0; y < img.Height; y++ {
+			c := img.At(x, y)
+			if c == 1 {
+				// Roll this rock north
+				img.Set(x, y, 0)
+				img.Set(x, next, 1)
+				rv += img.Height - next
+				next++
+			} else if c == 5 {
+				// Square rock
+				next = y + 1
+			}
+		}
+	}
+	return rv
+}
+
+func dec14rocks(c rune) int {
+	if c == '#' {
+		return 5
+	} else if c == 'O' {
+		return 1
+	}
+	return 0
+}
