af74850d7b33835514660eba6465f29cb1d3dab8
Thijs van Dijk <github@inurbanus.nl>
2023-12-15 06:36:02 +0100

    Solve 2023-15-A

--- ch/aoc23/dec15.go
+++ ch/aoc23/dec15.go
@@ -1,22 +1,32 @@
 package aoc23
 
 import (
+	"strings"
+
 	"github.com/thijzert/advent-of-code/ch"
 )
 
-var Dec15a ch.AdventFunc = nil
-// func Dec15a(ctx ch.AOContext) (interface{}, error) {
-// 	sections, err := ctx.DataSections("inputs/2023/dec15a.txt")
-// 	if err != nil {
-// 		return nil, err
-// 	}
-//
-// 	ctx.Print(len(sections))
-// 	return nil, errNotImplemented
-// }
+func Dec15a(ctx ch.AOContext) (interface{}, error) {
+	lines, err := ctx.DataLines("inputs/2023/dec15.txt")
+	if err != nil {
+		return nil, err
+	}
+	elems := strings.Split(lines[0], ",")
+
+	answer := 0
+	for _, elem := range elems {
+		h := byte(0)
+		for _, c := range elem {
+			h = 17 * (h + byte(c))
+		}
+		answer += int(h)
+	}
+
+	return answer, nil
+}
 
 var Dec15b ch.AdventFunc = nil
+
 // func Dec15b(ctx ch.AOContext) (interface{}, error) {
 // 	return nil, errNotImplemented
 // }
-
