9ac8ccaec56f95bb0483c7a2a18955513ddb44da
Thijs van Dijk <github@inurbanus.nl>
2023-12-16 09:20:40 +0100

    Solve 2023-16

--- ch/aoc23/dec16.go
+++ ch/aoc23/dec16.go
@@ -1,20 +1,18 @@
 package aoc23
 
 import (
+	"fmt"
+
 	"github.com/thijzert/advent-of-code/ch"
 	"github.com/thijzert/advent-of-code/lib/cube"
 	"github.com/thijzert/advent-of-code/lib/image"
 )
 
-type Beamfron struct {
+type Beamfront struct {
 	Pos, Dir cube.Point
 }
 
-func Dec16a(ctx ch.AOContext) (interface{}, error) {
-	lines, err := ctx.DataLines("inputs/2023/dec16.txt")
-	if err != nil {
-		return nil, err
-	}
+func dec16(lines []string, start Beamfront) int {
 	img := image.NewImage(len(lines[0]), len(lines), func(_, _ int) int {
 		return 0
 	})
@@ -46,8 +44,8 @@ func Dec16a(ctx ch.AOContext) (interface{}, error) {
 	mirrors[bfm{left, '|'}] = []cube.Point{up, down}
 	mirrors[bfm{down, '|'}] = []cube.Point{down}
 
-	beams := []Beamfron{Beamfron{cube.Point{-1, 0}, right}}
-	newBeams := []Beamfron{}
+	beams := []Beamfront{start}
+	newBeams := []Beamfront{}
 	moving := 1
 	for moving > 0 {
 		moving = 0
@@ -74,20 +72,19 @@ func Dec16a(ctx ch.AOContext) (interface{}, error) {
 
 			dirs := mirrors[bfm{beam.Dir, lines[beam.Pos.Y][beam.Pos.X]}]
 			if len(dirs) == 0 {
-				ctx.Printf("%d %d: '%c'", beam.Pos, beam.Dir, lines[beam.Pos.Y][beam.Pos.X])
-				return nil, errFailed
+				panic(fmt.Sprintf("%d %d: '%c'", beam.Pos, beam.Dir, lines[beam.Pos.Y][beam.Pos.X]))
 			}
 			beam.Dir = dirs[0]
 			beams[i] = beam
 			for _, d := range dirs[1:] {
-				newBeams = append(newBeams, Beamfron{beam.Pos, d})
+				newBeams = append(newBeams, Beamfront{beam.Pos, d})
 			}
 		}
 		beams = append(beams, newBeams...)
 		newBeams = newBeams[:0]
 	}
 
-	ctx.Printf("energised:\n%s", img)
+	//ctx.Printf("energised:\n%s", img)
 	answer := 0
 	for y := range lines {
 		for x := range lines[0] {
@@ -96,11 +93,63 @@ func Dec16a(ctx ch.AOContext) (interface{}, error) {
 			}
 		}
 	}
+	return answer
+}
+
+func Dec16a(ctx ch.AOContext) (interface{}, error) {
+	lines, err := ctx.DataLines("inputs/2023/dec16.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	answer := dec16(lines, Beamfront{
+		Pos: cube.Point{X: -1, Y: 0},
+		Dir: cube.Point{X: 1, Y: 0},
+	})
 	return answer, nil
 }
 
-var Dec16b ch.AdventFunc = nil
+func Dec16b(ctx ch.AOContext) (interface{}, error) {
+	lines, err := ctx.DataLines("inputs/2023/dec16.txt")
+	if err != nil {
+		return nil, err
+	}
+	w, h := len(lines[0]), len(lines)
 
-// func Dec16b(ctx ch.AOContext) (interface{}, error) {
-// 	return nil, errNotImplemented
-// }
+	answer, best := 0, Beamfront{}
+
+	bf0 := Beamfront{cube.Point{-1, 0}, cube.Point{1, 0}}
+	bf1 := Beamfront{cube.Point{w, 0}, cube.Point{-1, 0}}
+	for y := range lines {
+		bf0.Pos.Y = y
+		energised := dec16(lines, bf0)
+		if energised > answer {
+			answer, best = energised, bf0
+		}
+
+		bf1.Pos.Y = y
+		energised = dec16(lines, bf1)
+		if energised > answer {
+			answer, best = energised, bf1
+		}
+	}
+
+	bf0 = Beamfront{cube.Point{0, -1}, cube.Point{0, 1}}
+	bf1 = Beamfront{cube.Point{0, h}, cube.Point{0, -1}}
+	for x := range lines[0] {
+		bf0.Pos.X = x
+		energised := dec16(lines, bf0)
+		if energised > answer {
+			answer, best = energised, bf0
+		}
+
+		bf1.Pos.X = x
+		energised = dec16(lines, bf1)
+		if energised > answer {
+			answer, best = energised, bf1
+		}
+	}
+
+	ctx.Printf("Best option: starting at %d heading %d", best.Pos, best.Dir)
+	return answer, nil
+}
